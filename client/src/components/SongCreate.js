d69f2739880d53aa771d37d2d5d4dc7de2f34e55
Anton <antonliashok@gmail.com>
2019-03-10 11:54:36 +0200

    add: create-title component

--- client/src/components/SongCreate.js
+++ client/src/components/SongCreate.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
 
 export default class SongCreate extends Component {
+  
+  state = {
+    title: ''
+  }
+
   render() {
+    const { title } = this.state
     return (
-      <div>
-        new song
-      </div>
+      <>
+        <h3>add song</h3>
+        <form>
+          <input 
+            value={title}
+            onChange={ e => this.setState({ title: e.target.value }) }
+          />
+        </form>
+      </>
     )
   }
 }
